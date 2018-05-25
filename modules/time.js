33e21d62c287857693795667caa0fe895098843e
Krystian Bieniek <krystekk2013@gmail.com>
2018-05-25 15:32:54 +0200

    Add app, modules and time.js

---
+++ modules/time.js
@@ -0,0 +1,11 @@
+function changeTime(num) {
+  var hours = Math.floor(num / 3600);
+  var minutes = Math.floor(num / 60) - (hours * 60);
+  var sec = (num % 60).toFixed(0);
+  if (num < 3600) {
+    return minutes + 'min ' + sec + 'sec';
+  } else 
+  return hours + 'h ' + minutes + 'min ' + sec + 'sec';
+}
+
+exports.time = changeTime();
